a03d358eb3c1ed993c21d1cd06afec1a86f95ce9
Brandon Ly <wowbob396@gmail.com>
2020-11-23 17:01:33 -0600

    modified homepage

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! Sweet!</Text>
+      <TextInput></TextInput>
       <StatusBar style="auto" />
     </View>
   );
